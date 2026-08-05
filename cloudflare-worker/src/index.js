ce708a5dbd3692a4ec62a74553ef6bd361a8ea96
Devin Francis <31450006+DFrancis84@users.noreply.github.com>
2026-08-05 00:16:12 -0400

    Add URL handling for public cleaning requests

--- cloudflare-worker/src/index.js
+++ cloudflare-worker/src/index.js
@@ -58,6 +58,18 @@ export default {
           HttpError
         );
       }
+
+      const url = new URL(request.url);
+
+      if (isPublicCleaningRequest(request, url)) {
+        return await handlePublicCleaningRequest(
+          request,
+          env,
+          origin,
+          jsonResponse,
+          HttpError
+        );
+      }
       
       const jwt = request.headers.get("Cf-Access-Jwt-Assertion");
       if (!jwt) {
